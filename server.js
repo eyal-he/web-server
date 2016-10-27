e44767ecf67b58310dc8b001cd0f09e4bf9defb6
Eyal Herman <herman.eyal@gmail.com>
2016-10-27 15:13:24 +0300

    bla bla description

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require ('express');
+var app = express ();
+
+app.get('/', function(req, res) {
+	res.send('Hello Express!');
+	});
+
+app.get('/about', function(req, res) {
+	res.send('About us!');
+	});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000,function() {
+	console.log('it started!');
+});
